2545cb56efb040a5f5eac5b9036d02543a28a157
EPDollinger <emmanuel.dollinger@gmail.com>
2023-07-07 16:00:16 -0700

    Changed FDR

--- BigSur/feature_selection.py
+++ BigSur/feature_selection.py
@@ -11,7 +11,6 @@ import numpy as np
 from scipy.optimize import brentq
 from anndata import AnnData
 from mpmath import ncdf, exp
-from statsmodels.stats.multitest import fdrcorrection
 import numexpr as ne
 import warnings
 from .preprocessing import make_vars_and_qc, calculate_residuals, fit_cv, calculate_mcfano
@@ -182,7 +181,7 @@ def calculate_p_value(
                         indices, subsetemat, k2, k3, k4)
 
     # FDR correct:
-    _, p_vals_corrected_sub = fdrcorrection(p_vals[indices], alpha=cutoff)
+    _, p_vals_corrected_sub = fdrcorrection(p_vals[indices], p_vals.shape[0], alpha=cutoff) # For FDR just need to add a parameter to account for all pvalues
     p_vals[indices] = p_vals_corrected_sub
     meets_cutoff = np.repeat(False, p_vals.shape[0])
     # has nan for pvalues that weren't calculated
@@ -321,3 +320,103 @@ def find_pvals(corrected_fanos, p_vals, indices, subsetemat, k2, k3, k4):
         p_vals[indices[i]] = cdf_brent
 
     return p_vals
+
+def fdrcorrection(pvals, total_n_features, alpha=0.05, method='indep', is_sorted=False):
+    '''
+    The following code is from statsmodels.stats.multitest, v0.13.5. The only change is the _ecdf function. 
+    The new function allows for FDR correction given more tests than were actually computed.
+
+    pvalue correction for false discovery rate.
+
+    This covers Benjamini/Hochberg for independent or positively correlated and
+    Benjamini/Yekutieli for general or negatively correlated tests.
+
+    Parameters
+    ----------
+    pvals : array_like, 1d
+        Set of p-values of the individual tests.
+    total_n_features : int
+        Number of total tests that were run.
+    alpha : float, optional
+        Family-wise error rate. Defaults to ``0.05``.
+    method : {'i', 'indep', 'p', 'poscorr', 'n', 'negcorr'}, optional
+        Which method to use for FDR correction.
+        ``{'i', 'indep', 'p', 'poscorr'}`` all refer to ``fdr_bh``
+        (Benjamini/Hochberg for independent or positively
+        correlated tests). ``{'n', 'negcorr'}`` both refer to ``fdr_by``
+        (Benjamini/Yekutieli for general or negatively correlated tests).
+        Defaults to ``'indep'``.
+    is_sorted : bool, optional
+        If False (default), the p_values will be sorted, but the corrected
+        pvalues are in the original order. If True, then it assumed that the
+        pvalues are already sorted in ascending order.
+
+    Returns
+    -------
+    rejected : ndarray, bool
+        True if a hypothesis is rejected, False if not
+    pvalue-corrected : ndarray
+        pvalues adjusted for multiple hypothesis testing to limit FDR
+
+    Notes
+    -----
+    If there is prior information on the fraction of true hypothesis, then alpha
+    should be set to ``alpha * m/m_0`` where m is the number of tests,
+    given by the p-values, and m_0 is an estimate of the true hypothesis.
+    (see Benjamini, Krieger and Yekuteli)
+
+    The two-step method of Benjamini, Krieger and Yekutiel that estimates the number
+    of false hypotheses will be available (soon).
+
+    Both methods exposed via this function (Benjamini/Hochberg, Benjamini/Yekutieli)
+    are also available in the function ``multipletests``, as ``method="fdr_bh"`` and
+    ``method="fdr_by"``, respectively.
+
+    See also
+    --------
+    multipletests
+
+    '''
+    pvals = np.asarray(pvals)
+    assert pvals.ndim == 1, "pvals must be 1-dimensional, that is of shape (n,)"
+
+    if not is_sorted:
+        pvals_sortind = np.argsort(pvals)
+        pvals_sorted = np.take(pvals, pvals_sortind)
+    else:
+        pvals_sorted = pvals  # alias
+
+    if method in ['i', 'indep', 'p', 'poscorr']:
+        ecdffactor = _ecdf(pvals_sorted, total_n_features)
+    elif method in ['n', 'negcorr']:
+        cm = np.sum(1./np.arange(1, len(pvals_sorted)+1))   #corrected this
+        ecdffactor = _ecdf(pvals_sorted, total_n_features) / cm
+##    elif method in ['n', 'negcorr']:
+##        cm = np.sum(np.arange(len(pvals)))
+##        ecdffactor = ecdf(pvals_sorted)/cm
+    else:
+        raise ValueError('only indep and negcorr implemented')
+    reject = pvals_sorted <= ecdffactor*alpha
+    if reject.any():
+        rejectmax = max(np.nonzero(reject)[0])
+        reject[:rejectmax] = True
+
+    pvals_corrected_raw = pvals_sorted / ecdffactor
+    pvals_corrected = np.minimum.accumulate(pvals_corrected_raw[::-1])[::-1]
+    del pvals_corrected_raw
+    pvals_corrected[pvals_corrected>1] = 1
+    if not is_sorted:
+        pvals_corrected_ = np.empty_like(pvals_corrected)
+        pvals_corrected_[pvals_sortind] = pvals_corrected
+        del pvals_corrected
+        reject_ = np.empty_like(reject)
+        reject_[pvals_sortind] = reject
+        return reject_, pvals_corrected_
+    else:
+        return reject, pvals_corrected
+    
+def _ecdf(x, total_n_features):
+    '''no frills empirical cdf used in fdrcorrection; This function has been changed from statsmodels.stats.multitest, v0.13.5.
+    '''
+    nobs = len(x)
+    return np.arange(1,nobs+1)/float(total_n_features)
