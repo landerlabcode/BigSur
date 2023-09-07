134bb9b497ae15965616a16bc3d413965ddff701
EPDollinger <emmanuel.dollinger@gmail.com>
2023-09-07 12:05:13 -0700

    Deleted comment

--- BigSur/feature_selection.py
+++ BigSur/feature_selection.py
@@ -169,7 +169,7 @@ def solve_CF(c1, c2, c3, c4, c5):
         if real_roots.shape[0] == 0: ## If there are no real roots, set pvalue to 0.5
             cdf = 0.5
         else:
-            desired_root = min([abs(i) for i in real_roots]) # won't this return negative roots?
+            desired_root = min([abs(i) for i in real_roots])
             if desired_root >=8:
                 cdf = 0.5 * exp(-(np.longdouble(desired_root)**2) / 2)
             else:
