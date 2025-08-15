ad58cc6dd6fc0e822b70f9ee4f88277348147751
EPDollinger <emmanuel.dollinger+sub@pm.me>
2025-08-15 12:09:44 -0700

    Updated correlations

--- src/BigSur/correlations.py
+++ src/BigSur/correlations.py
@@ -18,6 +18,13 @@ from .preprocessing import make_vars_and_qc, calculate_residuals, fit_cv, calcul
 
 warnings.simplefilter('always', UserWarning)
 
+def calculate_correlations(batch_dict):
+        n_cells = batch_dict['All']['Barcodes'].shape[0]
+        mc_fanos = batch_dict['All']['mcFanos']
+        mcPCCs = 1/((n_cells - 1) * np.sqrt(mc_fanos * mc_fanos.T)) * (batch_dict['All']['Residuals'].T @ batch_dict['All']['Residuals'])
+
+        batch_dict['mcPCCs'] = mcPCCs
+
 
 def calculate_mcPCCs(adata, layer, verbose = 1, batch_key: str = None, cv = None):
     '''Calculate modified Pearson correlation coefficients (mcPCCs) for each gene pair in the dataset.'''
@@ -27,32 +34,6 @@ def calculate_mcPCCs(adata, layer, verbose = 1, batch_key: str = None, cv = None
     batch_dict = make_vars_and_qc(adata, layer, batch_key = batch_key)
         
     tic = time.perf_counter()
-    # Fit cv if not provided
-    if cv is None:
-        if verbose > 1:
-            print('Fitting cv.')
-        fit_cv(batch_dict, verbose)
-    elif isinstance(cv, float):
-        if batch_key is not None:
-            raise Exception("Batch key was provided, but cv only has one value. Please pass cv as a dict of {batch:cv} pairs.")
-        batch_dict['All']['CV'] = cv
-    elif isinstance(cv, dict):
-        if not cv.keys() <= batch_dict.keys():
-            raise Exception("If providing CV as a dict, keys must match batch names.")
-        for batch in cv:
-            batch_dict[batch]['CV'] = cv[batch]
-            
-    if verbose > 1:
-        for batch in batch_dict:
-            # If batch is 'All' and there's more than one batch, skip it
-            if (len(batch_dict) > 1) and (batch == 'All'):
-                continue
-            # If batch is 'All' and there's only one batch, print its CV
-            elif (batch == 'All') and (len(batch_dict) == 1):
-                print(f"Using a coefficient of variation of {batch_dict[batch]['CV']:.4}.")
-            # If batch is not 'All', print its CV
-            else:
-                print(f"Using a coefficient of variation of {batch_dict[batch]['CV']:.4} for batch {batch}.")
     # Calculate residuals
     calculate_residuals(batch_dict)
 
@@ -72,24 +53,14 @@ def calculate_mcPCCs(adata, layer, verbose = 1, batch_key: str = None, cv = None
         )
 
     # Store mc_Fano and cv
-    mc_fanos = np.array(batch_dict['All']['mcFanos']).flatten()
-    adata.var["mc_Fano"] = mc_fanos
-
-    for batch in batch_dict:
-        # If batch is 'All' and there's more than one batch, skip it
-        if (len(batch_dict) > 1) and (batch == 'All'):
-            continue
-        # If batch is 'All' and there's only one batch, print its CV
-        elif (batch == 'All') and (len(batch_dict) == 1):
-            adata.uns['CV_for_mc_Fano_fit'] = batch_dict['All']['CV']
-        else:
-            adata.uns[f'CV_for_mc_Fano_fit_{batch}'] = batch_dict[batch]['CV']
+    adata.var["mc_Fano"] = np.array(batch_dict['All']['mcFanos']).flatten()
 
     # Calculate mcPCCs
     calculate_correlations(batch_dict)
 
-    def calculate_correlations(batch_dict):
-        n_cells = batch_dict['All']['Barcodes'].shape[0]
-        mcPCCs = 1/((n_cells - 1) * np.sqrt(mc_fanos * mc_fanos.T)) * (batch_dict['All']['Residuals'].T @ batch_dict['All']['Residuals'])
 
-        adata.varm['mcPCCs'] = mcPCCs
+    adata.varm['mcPCCs'] = batch_dict['mcPCCs']
+
+    return adata
+
+    
